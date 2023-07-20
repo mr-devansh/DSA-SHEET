7ebdb709d1f5a7fda1eb47b8ae9b5c204133c269
Devansh Singh <devu0996@gmail.com>
2023-07-20 21:36:49 +0530

    Update and rename find common elements three sorted arrays.java to find_common_elements_three_sorted_arrays.java

--- SEARCHING&SORTING/find_common_elements_three_sorted_arrays.java
+++ SEARCHING&SORTING/find_common_elements_three_sorted_arrays.java
@@ -1,22 +1,22 @@
-
 class Solution {
     ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) {
         ArrayList<Integer> list = new ArrayList<>();
         int i = 0, j = 0, k = 0;
-        int prevA = Integer.MIN_VALUE, prevB = Integer.MIN_VALUE, prevC = Integer.MIN_VALUE;
+        int prev = Integer.MIN_VALUE;
 
         while (i < n1 && j < n2 && k < n3) {
+            // If A[i] == B[j] == C[k], add to the list and move all three pointers.
             if (A[i] == B[j] && B[j] == C[k]) {
-                if (A[i] != prevA) {
+                // Check for duplicates
+                if (A[i] != prev) {
                     list.add(A[i]);
-                    prevA = A[i];
-                    prevB = B[j];
-                    prevC = C[k];
+                    prev = A[i];
                 }
                 i++;
                 j++;
                 k++;
             } else if (A[i] <= B[j] && A[i] <= C[k]) {
+                // Move the pointer in the smallest array
                 i++;
             } else if (B[j] <= A[i] && B[j] <= C[k]) {
                 j++;
