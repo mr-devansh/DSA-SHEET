c13d46d7ed163a58ee28ad15bd4f9c2b1591a1b3
Devansh Singh <devu0996@gmail.com>
2023-08-14 21:55:41 +0530

    Update Repeat and Missing Number Array.java

--- ARRAYS/Repeat and Missing Number Array.java
+++ ARRAYS/Repeat and Missing Number Array.java
@@ -1,26 +1,21 @@
 public class Solution {
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int [] repeatedNumber(final int[] A) {
-        int []out=new int[2];
-        int n=A.length;
-        int[] temp= new int[n]; 
-        int repeatingNumber = -1;
-        int missingNumber = -1;
-        Arrays.sort(A);
-        for (int i = 0; i < n; i++) {
-            temp[A[i] - 1]++;
-            if (temp[A[i] - 1] > 1) {
-                repeatingNumber = A[i];
+        int ans[] = new int[2];
+        for(int i = 0; i< A.length; i++){
+            if(A[Math.abs(A[i])-1]<0){
+                ans[0] = Math.abs(A[i]);
+            }
+            else{
+                A[Math.abs(A[i])-1] = -A[Math.abs(A[i])-1];
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (temp[i] == 0) {
-                missingNumber = i + 1;
+        for(int i = 0; i<A.length; i++){
+            if(A[i]>0){
+                ans[1] = i+1;
                 break;
             }
         }
-        out[0]=repeatingNumber;
-        out[1]=missingNumber;
-        return out;
+        return ans;
     }
 }
