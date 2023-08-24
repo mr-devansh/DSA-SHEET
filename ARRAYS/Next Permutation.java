51ec1911b4039baf03716bef100d9bba87ba0396
Devansh Singh <devu0996@gmail.com>
2023-08-24 17:58:44 +0530

    medium level questions started

--- ARRAYS/Next Permutation.java
+++ ARRAYS/Next Permutation.java
@@ -1,25 +1,27 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         if(nums.length<=1 || nums==null) return;
-
         int i = nums.length-2;
-        while(i>=0 && nums[i]>=nums[i+1])i--;
-
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
         if(i>=0){
             int j = nums.length-1;
-            while(nums[j]<=nums[i])j--;
-            swap(nums, i ,j);
+            while(j>i && nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums, i , j);
         }
-        reverse(nums, i+1 , nums.length-1);
+        reverse(nums, i+1, nums.length-1);
     }
-    public static void reverse(int[] nums, int i, int j){
+    public void swap(int[] nums, int i ,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void reverse(int nums[], int i, int j){
         while(i<j){
-            swap(nums,i++,j--);
+            swap(nums, i++ , j--);
         }
-    }
-    public static void swap(int[] nums, int i, int j){
-        int temp =nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
