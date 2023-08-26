20c674c47976f5732183f5cdde74bfbcae104977
Devansh Singh <devu0996@gmail.com>
2023-08-26 20:52:40 +0530

    Update Find Minimum Number of Merge Operations to Make an Array Palindrome.java

--- ARRAYS/Find Minimum Number of Merge Operations to Make an Array Palindrome.java
+++ ARRAYS/Find Minimum Number of Merge Operations to Make an Array Palindrome.java
@@ -1,53 +1,22 @@
-// Java program to find number of operations
-// to make an array palindrome
-
-class GFG
-{
-	// Returns minimum number of count operations
-	// required to make arr[] palindrome
-	static int findMinOps(int[] arr, int n)
-	{
-		int ans = 0; // Initialize result
-
-		// Start from two corners
-		for (int i=0,j=n-1; i<=j;)
-		{
-			// If corner elements are same,
-			// problem reduces arr[i+1..j-1]
-			if (arr[i] == arr[j])
-			{
-				i++;
-				j--;
-			}
-
-			// If left element is greater, then
-			// we merge right two elements
-			else if (arr[i] > arr[j])
-			{
-				// need to merge from tail.
-				j--;
-				arr[j] += arr[j+1] ;
-				ans++;
-			}
-
-			// Else we merge left two elements
-			else
-			{
-				i++;
-				arr[i] += arr[i-1];
-				ans++;
-			}
-		}
-
-		return ans;
-	}
-
-	// Driver method to test the above function
-	public static void main(String[] args)
-	{
-		int arr[] = new int[]{1, 4, 5, 9, 1} ;
-		System.out.println("Count of minimum operations is "+
-								findMinOps(arr, arr.length));
-	
-	}
-}
+static int findMinOps(int[] arr, int n){
+        int i = 0;
+        int j =n-1;
+        int ans = 0;
+        while(i<=j){
+            if(arr[i]==arr[j]){
+                i++;
+                j--;
+            }
+            else if(arr[i]<arr[j]){
+                i++;
+                arr[i]+=arr[i-1];
+                ans++;
+            }
+            else{
+                j--;
+                arr[j]+=arr[j+1];
+                ans++;
+            }
+        }
+        return ans;
+    }
